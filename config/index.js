50b497c4372683dbf950a0d3f019e5ba8a793bfa
Andrey Pavlov <paffa4@gmail.com>
2014-06-27 10:55:24 +0000

    For use proccess enviroment port

--- config/index.js
+++ config/index.js
@@ -5,4 +5,6 @@ nconf.argv()
   .env()
   .file({ file: path.join(__dirname, 'config.json') });
 
+nconf.stores.file.store.port = process.env.PORT || nconf.stores.file.store.port;
+
 module.exports = nconf;

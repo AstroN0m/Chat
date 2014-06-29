5c78a05a9ba7c121c3f79d72f3505dcbc74bf089
Andrey Pavlov <paffa4@gmail.com>
2014-06-29 08:38:57 +0000

    Config file for RequireJS

---
+++ public/js/requirejs-config.js
@@ -0,0 +1,10 @@
+var requirejs = {
+    baseUrl: "/js/",
+    paths: {
+        jquery: '../vendor/bower_components/jquery/dist/jquery.min',
+        bootstrap: '../vendor/bower_components/bootstrap/dist/js/bootstrap.min',
+        io: '../vendor/bower_components/socket.io-client/dist/socket.io'
+    },
+    shim: {
+    }
+};
